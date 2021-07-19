ac05fdf7a7276355a3beba78784682626e87019b
guyyablonka <guyyablonka@gmail.com>
2021-07-19 06:07:44 +0300

    added authentication user with server to db

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -10,7 +10,7 @@ let USERS = [
     password: "1234",
     adress: "Petah Tikva, Tor Azahav 35",
     phone: "0509845887",
-    isDesiner: true,
+    roles: ["user", "admin", "designer"],
     registerDate: "12/10/2020",
     image:
       "https://image.cnbcfm.com/api/v1/image/100496736-steve-jobs-march-2011-getty.jpg?v=1513863842&w=1400&h=950",
@@ -19,6 +19,8 @@ let USERS = [
       cvv: "123",
       expiredDate: "12/32",
     },
+    orders: [],
+    products: [],
   },
 ];
 
@@ -41,44 +43,26 @@ const getUserById = (req, res, next) => {
   res.json({ user });
 };
 
-const connectUser = (req, res, next) => {
-  const { username, password } = req.body;
+const connectUser = async (req, res, next) => {
+  const { email, password } = req.body;
 
-  const identifiedUser = USERS.find((user) => user.username === username);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Singing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Could not identify user.", 401);
+  if (!existingUser || existingUser.password !== password) {
+    const error = HttpError("Invalid credential could not log you in.", 401);
+    return next(error);
   }
 
   res.json({ message: "logged in!" });
-};
-
-const addUser = (req, res, next) => {
-  const {
-    username,
-    email,
-    password,
-    adress,
-    phone,
-    isDesiner,
-    registerDate,
-    image,
-  } = req.body;
-
-  const createdUser = {
-    id: uuid(),
-    username,
-    email,
-    password,
-    adress,
-    phone,
-    isDesiner,
-    registerDate,
-    image,
-  };
-
-  USERS.push(createdUser);
-  res.status(201).json({ user: createdUser });
 };
 
 const updateUser = (req, res, next) => {
@@ -107,8 +91,47 @@ const updateUser = (req, res, next) => {
   res.status(200).json({ user: updatedUser });
 };
 
+const signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+  const { username, password, email } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Singing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  if (existingUser) {
+    const error = new HttpError(
+      "User exists already, please login instead.",
+      422
+    );
+    return next(error);
+  }
+
+  const createdUser = new User(USERS[0]);
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Creating user failed, please try again.", 500);
+
+    return next(error);
+  }
+
+  res.status(201).json({ user: createdUser });
+};
+
+exports.signup = signup;
 exports.updateUser = updateUser;
 exports.connectUser = connectUser;
-exports.addUser = addUser;
 exports.getAllUsers = getAllUsers;
 exports.getUserById = getUserById;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
-
+const uniqueValidatior = require("mongoose-unique-validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  username: { type: String, required: false },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: false, minLength: 6 },
   adress: { type: String, required: true },
   phone: { type: String, required: true },
-  isDesiner: { type: Boolean, required: true },
+  roles: { type: Array, required: true },
   registerDate: { type: Date, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   creditCard: {
     number: { type: String, required: true },
     cvv: { type: String, required: true },
     expiredDate: { type: String, required: true },
   },
+  orders: { type: Array, required: true },
+  products: { type: Array, required: false },
 });
+
+userSchema.plugin(uniqueValidatior);
 
 module.exports = mongoose.model("User", userSchema);
